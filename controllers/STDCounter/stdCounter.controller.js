ac741cafff847d6389854b385ed55934bc9143c7
bambangW06 <bambang.alias@gmail.com>
2024-10-21 15:33:00 +0700

    update

--- controllers/STDCounter/stdCounter.controller.js
+++ controllers/STDCounter/stdCounter.controller.js
@@ -3,11 +3,15 @@ module.exports = {
   getSTDCounter: async (req, res) => {
     try {
       const id = req.params.id;
-      const q = `SELECT std_counter FROM tb_m_master_tools WHERE std_counter_id = $1`;
+
+      console.log("id", id);
+      const q = `SELECT std_counter FROM tb_m_master_tools WHERE tool_id= $1`;
       const client = await database.connect();
       const userDataQuery = await client.query(q, [id]);
       const userData = userDataQuery.rows;
       client.release();
+      console.log("userData", userData);
+
       res.status(200).json({
         message: "Success to Get Data",
         data: userData,
@@ -16,6 +20,30 @@ module.exports = {
       res.status(500).json({
         message: "Failed to Get Data",
         error: error,
+      });
+    }
+  },
+  getTimeRanges: async (req, res) => {
+    try {
+      const shift = req.params.shift;
+      console.log("shift", shift);
+
+      const q = `SELECT * FROM tb_m_time_reports WHERE shift = $1`;
+      const values = [shift];
+      const client = await database.connect();
+      const userDataQuery = await client.query(q, values);
+      const userData = userDataQuery.rows;
+      client.release();
+
+      console.log("userData", userData);
+      res.status(200).json({
+        message: "Success to Get Data",
+        data: userData,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Failed to Get Data",
+        error: error.message, // Kirim pesan error yang lebih jelas
       });
     }
   },
--- routes/StdCounter/stdCounter.js
+++ routes/StdCounter/stdCounter.js
@@ -1,9 +1,11 @@
 const {
   getSTDCounter,
+  getTimeRanges,
 } = require("../../controllers/STDCounter/stdCounter.controller");
 
 var router = require("express").Router();
 
 router.get("/get/:id", getSTDCounter);
+router.get("/timeranges/:shift", getTimeRanges);
 
 module.exports = router;
